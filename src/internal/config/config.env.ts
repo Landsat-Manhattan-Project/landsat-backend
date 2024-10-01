4e54be6b6814256539ed4491e974e43dc62936c0
Richard Lion Guevara Puerta <leonvilla6@outlook.com>
2024-09-30 21:54:34 -0500

    Fix setuo for mongo local

--- src/internal/config/config.env.ts
+++ src/internal/config/config.env.ts
@@ -4,5 +4,8 @@ dotenv.config();
 export const config = {
   port: process.env.PORT ?? 8080,
   jwtSecret: process.env.JWT_SECRET ?? 'jwt-example-key',
-  mongoUri: process.env.MONGO_URI ?? 'mongodb://rootuser:rootpassword@localhost:27017/landsat-db?authSource=admin'
+  mongoUri: process.env.MONGO_URI ?? 'mongodb://rootuser:rootpassword@localhost:27017/landsat-db?authSource=admin',
+  privateKeyPath: process.env.PRIVATE_KEY_PATH ?? 'private.pem',
+  publicKeyPath: process.env.PUBLIC_KEY_PATH ?? 'public.pem',
+  expirationTime: process.env.EXPIRATION_TIME ?? '14d'
 };
